1c97a5ec3511f612cc3d5c65f84a97c9ec8f4479
Aries Thio <aries.thio@hotmail.com>
2024-04-06 13:53:57 +0200

    Refactored orderTable.js and added moment

--- components/coinbase/OrderTable.jsx
+++ components/coinbase/OrderTable.jsx
@@ -19,226 +19,27 @@ import {
 } from "@nextui-org/react";
 import { ChevronDownIcon, PlusIcon, SearchIcon } from "lucide-react";
 import React from "react";
+import * as moment from "moment";
 
 const columns = [
   { name: "ID", uid: "id", sortable: true },
-  { name: "ASSET", uid: "name", sortable: true },
+  { name: "ASSET", uid: "asset", sortable: true },
+  { name: "TYPE", uid: "type", sortable: true },
   { name: "AMOUNT", uid: "amount", sortable: true },
+  { name: "PRICE", uid: "price", sortable: true },
   { name: "BALANCE", uid: "balance", sortable: true },
-  { name: "COST", uid: "cost" },
+  { name: "TIMESTAMP", uid: "timestamp" },
   { name: "ACTIONS", uid: "actions" },
 ];
 
-const users = [
-  {
-    id: 1,
-    name: "Tony Reichert",
-    role: "CEO",
-    team: "Management",
-    status: "active",
-    age: "29",
-    avatar: "https://i.pravatar.cc/150?u=a042581f4e29026024d",
-    email: "tony.reichert@example.com",
-  },
-  {
-    id: 2,
-    name: "Zoey Lang",
-    role: "Tech Lead",
-    team: "Development",
-    status: "paused",
-    age: "25",
-    avatar: "https://i.pravatar.cc/150?u=a042581f4e29026704d",
-    email: "zoey.lang@example.com",
-  },
-  {
-    id: 3,
-    name: "Jane Fisher",
-    role: "Sr. Dev",
-    team: "Development",
-    status: "active",
-    age: "22",
-    avatar: "https://i.pravatar.cc/150?u=a04258114e29026702d",
-    email: "jane.fisher@example.com",
-  },
-  {
-    id: 4,
-    name: "William Howard",
-    role: "C.M.",
-    team: "Marketing",
-    status: "vacation",
-    age: "28",
-    avatar: "https://i.pravatar.cc/150?u=a048581f4e29026701d",
-    email: "william.howard@example.com",
-  },
-  {
-    id: 5,
-    name: "Kristen Copper",
-    role: "S. Manager",
-    team: "Sales",
-    status: "active",
-    age: "24",
-    avatar: "https://i.pravatar.cc/150?u=a092581d4ef9026700d",
-    email: "kristen.cooper@example.com",
-  },
-  {
-    id: 6,
-    name: "Brian Kim",
-    role: "P. Manager",
-    team: "Management",
-    age: "29",
-    avatar: "https://i.pravatar.cc/150?u=a042581f4e29026024d",
-    email: "brian.kim@example.com",
-    status: "Active",
-  },
-  {
-    id: 7,
-    name: "Michael Hunt",
-    role: "Designer",
-    team: "Design",
-    status: "paused",
-    age: "27",
-    avatar: "https://i.pravatar.cc/150?u=a042581f4e29027007d",
-    email: "michael.hunt@example.com",
-  },
-  {
-    id: 8,
-    name: "Samantha Brooks",
-    role: "HR Manager",
-    team: "HR",
-    status: "active",
-    age: "31",
-    avatar: "https://i.pravatar.cc/150?u=a042581f4e27027008d",
-    email: "samantha.brooks@example.com",
-  },
-  {
-    id: 9,
-    name: "Frank Harrison",
-    role: "F. Manager",
-    team: "Finance",
-    status: "vacation",
-    age: "33",
-    avatar: "https://i.pravatar.cc/150?img=4",
-    email: "frank.harrison@example.com",
-  },
-  {
-    id: 10,
-    name: "Emma Adams",
-    role: "Ops Manager",
-    team: "Operations",
-    status: "active",
-    age: "35",
-    avatar: "https://i.pravatar.cc/150?img=5",
-    email: "emma.adams@example.com",
-  },
-  {
-    id: 11,
-    name: "Brandon Stevens",
-    role: "Jr. Dev",
-    team: "Development",
-    status: "active",
-    age: "22",
-    avatar: "https://i.pravatar.cc/150?img=8",
-    email: "brandon.stevens@example.com",
-  },
-  {
-    id: 12,
-    name: "Megan Richards",
-    role: "P. Manager",
-    team: "Product",
-    status: "paused",
-    age: "28",
-    avatar: "https://i.pravatar.cc/150?img=10",
-    email: "megan.richards@example.com",
-  },
-  {
-    id: 13,
-    name: "Oliver Scott",
-    role: "S. Manager",
-    team: "Security",
-    status: "active",
-    age: "37",
-    avatar: "https://i.pravatar.cc/150?img=12",
-    email: "oliver.scott@example.com",
-  },
-  {
-    id: 14,
-    name: "Grace Allen",
-    role: "M. Specialist",
-    team: "Marketing",
-    status: "active",
-    age: "30",
-    avatar: "https://i.pravatar.cc/150?img=16",
-    email: "grace.allen@example.com",
-  },
-  {
-    id: 15,
-    name: "Noah Carter",
-    role: "IT Specialist",
-    team: "I. Technology",
-    status: "paused",
-    age: "31",
-    avatar: "https://i.pravatar.cc/150?img=15",
-    email: "noah.carter@example.com",
-  },
-  {
-    id: 16,
-    name: "Ava Perez",
-    role: "Manager",
-    team: "Sales",
-    status: "active",
-    age: "29",
-    avatar: "https://i.pravatar.cc/150?img=20",
-    email: "ava.perez@example.com",
-  },
-  {
-    id: 17,
-    name: "Liam Johnson",
-    role: "Data Analyst",
-    team: "Analysis",
-    status: "active",
-    age: "28",
-    avatar: "https://i.pravatar.cc/150?img=33",
-    email: "liam.johnson@example.com",
-  },
-  {
-    id: 18,
-    name: "Sophia Taylor",
-    role: "QA Analyst",
-    team: "Testing",
-    status: "active",
-    age: "27",
-    avatar: "https://i.pravatar.cc/150?img=29",
-    email: "sophia.taylor@example.com",
-  },
-  {
-    id: 19,
-    name: "Lucas Harris",
-    role: "Administrator",
-    team: "Information Technology",
-    status: "paused",
-    age: "32",
-    avatar: "https://i.pravatar.cc/150?img=50",
-    email: "lucas.harris@example.com",
-  },
-  {
-    id: 20,
-    name: "Mia Robinson",
-    role: "Coordinator",
-    team: "Operations",
-    status: "active",
-    age: "26",
-    avatar: "https://i.pravatar.cc/150?img=45",
-    email: "mia.robinson@example.com",
-  },
+const INITIAL_VISIBLE_COLUMNS = [
+  "asset",
+  "type",
+  "amount",
+  "price",
+  "balance",
+  "timestamp",
 ];
-
-const statusOptions = [
-  { name: "Active", uid: "active" },
-  { name: "Paused", uid: "paused" },
-  { name: "Vacation", uid: "vacation" },
-];
-
-const INITIAL_VISIBLE_COLUMNS = ["name", "amount", "balance", "cost"];
 
 export function OrderTable() {
   const { orders } = useCoinbase();
@@ -252,31 +53,22 @@ export function OrderTable() {
   );
 
   const [filterValue, setFilterValue] = React.useState("");
-  const [statusFilter, setStatusFilter] = React.useState("all");
 
   const hasSearchFilter = Boolean(filterValue);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [rowsPerPage, setRowsPerPage] = React.useState(30);
   const [page, setPage] = React.useState(1);
 
   const filteredItems = React.useMemo(() => {
-    let filteredUsers = [...users];
+    let filteredOrders = [...orders];
 
     if (hasSearchFilter) {
-      filteredUsers = filteredUsers.filter((user) =>
-        user.name.toLowerCase().includes(filterValue.toLowerCase()),
-      );
-    }
-    if (
-      statusFilter !== "all" &&
-      Array.from(statusFilter).length !== statusOptions.length
-    ) {
-      filteredUsers = filteredUsers.filter((user) =>
-        Array.from(statusFilter).includes(user.status),
+      filteredOrders = filteredOrders.filter((order) =>
+        order.product_id.toLowerCase().includes(filterValue.toLowerCase()),
       );
     }
 
-    return filteredUsers;
-  }, [users, filterValue, statusFilter]);
+    return filteredOrders;
+  }, [orders, filterValue]);
 
   const pages = Math.ceil(filteredItems.length / rowsPerPage);
 
@@ -341,34 +133,11 @@ export function OrderTable() {
             placeholder="Search by name..."
             startContent={<SearchIcon />}
             value={filterValue}
+            size="sm"
             onClear={() => onClear()}
             onValueChange={onSearchChange}
           />
           <div className="flex gap-3">
-            <Dropdown>
-              <DropdownTrigger className="hidden sm:flex">
-                <Button
-                  endContent={<ChevronDownIcon className="text-small" />}
-                  variant="flat"
-                >
-                  Status
-                </Button>
-              </DropdownTrigger>
-              <DropdownMenu
-                disallowEmptySelection
-                aria-label="Table Columns"
-                closeOnSelect={false}
-                selectedKeys={statusFilter}
-                selectionMode="multiple"
-                onSelectionChange={setStatusFilter}
-              >
-                {statusOptions.map((status) => (
-                  <DropdownItem key={status.uid} className="capitalize">
-                    {capitalize(status.name)}
-                  </DropdownItem>
-                ))}
-              </DropdownMenu>
-            </Dropdown>
             <Dropdown>
               <DropdownTrigger className="hidden sm:flex">
                 <Button
@@ -400,7 +169,7 @@ export function OrderTable() {
         </div>
         <div className="flex justify-between items-center">
           <span className="text-default-400 text-small">
-            Total {users.length} users
+            Total {orders.length} orders
           </span>
           <label className="flex items-center text-default-400 text-small">
             Rows per page:
@@ -408,9 +177,9 @@ export function OrderTable() {
               className="bg-transparent outline-none text-default-400 text-small"
               onChange={onRowsPerPageChange}
             >
-              <option value="5">5</option>
-              <option value="10">10</option>
-              <option value="15">15</option>
+              <option value="30">30</option>
+              <option value="50">50</option>
+              <option value="100">100</option>
             </select>
           </label>
         </div>
@@ -418,10 +187,9 @@ export function OrderTable() {
     );
   }, [
     filterValue,
-    statusFilter,
     visibleColumns,
     onRowsPerPageChange,
-    users.length,
+    orders.length,
     onSearchChange,
     hasSearchFilter,
   ]);
@@ -465,32 +233,46 @@ export function OrderTable() {
     );
   }, [selectedKeys, items.length, page, pages, hasSearchFilter]);
 
-  const renderCellOrder = React.useCallback((data, columnKey) => {
+  const renderCell = React.useCallback((data, columnKey) => {
     // console.log("Data, ", data);
     // console.log(columnKey);
     const cellValue = data[columnKey];
     // console.log(cellValue);
     switch (columnKey) {
-      case "name":
+      case "asset":
         return (
           <div className="flex flex-row items-center gap-x-4 my-2">
             <p className="text-md">{data.product_id}</p>
+          </div>
+        );
+      case "type":
+        return (
+          <div className="flex flex-row items-center gap-x-4 my-2">
             <p className="text-md">{data.side}</p>
           </div>
         );
       case "amount":
         return (
           <div className="flex flex-col">
-            <p className="text-bold text-small capitalize">{data.price}</p>
             <p className="text-bold text-tiny capitalize text-default-400">
               {data.size}
             </p>
           </div>
         );
+      case "price":
+        return (
+          <div className="flex flex-col">
+            <p className="text-bold text-small capitalize">{data.price}</p>
+          </div>
+        );
       case "balance":
         return <p className="text-md">{data.price * data.size}</p>;
-      case "cost":
-        return <p className="text-md">{data.trade_time}</p>;
+      case "timestamp":
+        return (
+          <p className="text-md">
+            {moment(data.trade_time).format("DD-MMM-YYYY HH:mm:ss.SSS")}
+          </p>
+        );
       // case "actions":
       //   return (
       //     <div className="relative flex justify-end items-center gap-2">
@@ -541,12 +323,12 @@ export function OrderTable() {
           </TableColumn>
         )}
       </TableHeader>
-      <TableBody emptyContent={"No users found"} items={orders}>
+      <TableBody emptyContent={"No orders found"} items={items}>
         {(item) => (
           <TableRow key={item.entry_id}>
             {(columnKey) => (
               <TableCell className="py-1">
-                {renderCellOrder(item, columnKey)}
+                {renderCell(item, columnKey)}
               </TableCell>
             )}
           </TableRow>
